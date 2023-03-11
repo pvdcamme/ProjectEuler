0cbe8ecc5010f563331eeb4350057244a9b7df18
pvdcamme <pvdcamme@gmail.com>
2023-03-11 22:40:34 +0000

    move to code out to a separate cache

--- src/solutions/Problem26.java
+++ src/solutions/Problem26.java
@@ -5,25 +5,33 @@ import java.util.WeakHashMap;
 import java.util.function.Function;
 
 public class Problem26 extends Problem {
+	private static final class Cache {
+		final WeakHashMap<Integer, BigInteger> seen = new WeakHashMap<>();
+		private Function<Integer, BigInteger> divisorFun;
+
+		Cache(int divisor) {
+			BigInteger bigDivisor = BigInteger.valueOf(divisor);
+			divisorFun = (Integer v) -> {
+				return BigInteger.TEN.modPow(BigInteger.valueOf(v), bigDivisor);
+			};
+		}
+
+		BigInteger get(int power) {
+			return seen.computeIfAbsent(power, divisorFun);
+		}
+	}
 
 	public Problem26() {
 		super(26);
 	}
 
-	int search(WeakHashMap<Integer, BigInteger> seen, int divisor, int skip, int searchLength) {
-		BigInteger bigDivisor = BigInteger.valueOf(divisor);
-		Function<Integer, BigInteger> divisorFun = (Integer v) -> {
-			return BigInteger.TEN.modPow(BigInteger.valueOf(v), bigDivisor);
-		};
+	int search(Cache seen, int divisor, int skip, int searchLength) {
 		for (int lengthCtr = 1; lengthCtr < searchLength; ++lengthCtr) {
-			BigInteger small = seen.computeIfAbsent(lengthCtr + skip, divisorFun);
-			BigInteger large = seen.computeIfAbsent(lengthCtr * 2 + skip, divisorFun);
-			if (!small.equals(large)) {
-				continue;
-			}
-			BigInteger gigantic = seen.computeIfAbsent(lengthCtr * 3 + skip, divisorFun);
+			BigInteger small = seen.get(lengthCtr + skip);
+			BigInteger large = seen.get(lengthCtr * 2 + skip);
+			BigInteger gigantic = seen.get(lengthCtr * 3 + skip);
 
-			if (small.equals(gigantic)) {
+			if (small.equals(large) && small.equals(gigantic)) {
 				return lengthCtr;
 			}
 		}
@@ -39,15 +47,15 @@ public class Problem26 extends Problem {
 
 			int attempt = 1 + (largestCycle / 10);
 			int length = -1;
-			WeakHashMap<Integer, BigInteger> seen = new WeakHashMap<>();
-			while (-1 == (length = search(seen, ctr, 2 * attempt, 2 * attempt)))
+
+			Cache cache = new Cache(ctr);
+			while (-1 == (length = search(cache, ctr, 2 * attempt, 2 * attempt)))
 				attempt++;
 
 			if (length > cycleLength) {
 				largestCycle = ctr;
 				cycleLength = length;
 			}
-
 		}
 		System.out.format("%s: : 1/%d has a reciprocal of %d digits\n", this, largestCycle, cycleLength);
 
