75f795c7cacb73a6f5b168b2ac0935f022a8efd5
pvdcamme <pvdcamme@gmail.com>
2023-03-12 13:50:47 +0000

    cleanup code, pick better variable names

--- src/solutions/Problem47.java
+++ src/solutions/Problem47.java
@@ -1,14 +1,5 @@
 package solutions;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import solutions.PrimeUtils.PrimeFactor;
-
-import java.util.HashMap;
-import java.util.HashSet;
-
 /**
  * Find the first four consecutive integers to have four distinct prime factors
  * each. What is the first of these numbers?
@@ -20,40 +11,40 @@ public class Problem47 extends Problem {
         super(47);
     }
 
-    public int boundedSearch(int minCount, int maxval) {
-        int[] cnt = new int[maxval];
-        PrimeSerie p = new PrimeSerie();
-        while (p.next() < maxval) {
-            int currentPrime = (int) p.current();
-            long val = currentPrime;
+    public int boundedSearch(int minCount, int maxSearch) {
+        int[] factorCount = new int[maxSearch];
+        PrimeSerie primes = new PrimeSerie();
+        while (primes.next() < maxSearch) {
+            int currentPrime = (int) primes.current();
+            int multiples = currentPrime;
 
-            while (val < maxval) {
-                cnt[(int) val] += 1;
-                val += currentPrime;
+            while (multiples < maxSearch) {
+                factorCount[multiples] += 1;
+                multiples += currentPrime;
             }
         }
 
-        for (int ctr = 0; ctr < (maxval - minCount); ++ctr) {
+        for (int firstPos = 0; firstPos < (maxSearch - minCount); ++firstPos) {
             boolean allGood = true;
-            for (int inner = 0; allGood && inner < minCount; ++inner) {
-                allGood = allGood && (cnt[ctr + inner] == minCount);
+            for (int neighbourIdx = 0; allGood && neighbourIdx < minCount; ++neighbourIdx) {
+                allGood = allGood && (factorCount[firstPos + neighbourIdx] == minCount);
             }
             if (allGood) {
-                return ctr;
+                return firstPos;
             }
         }
         return -1;
     }
 
     public void solve() {
-        int size = 10;
+        int searchSize = 10;
         while (true) {
-            int res = boundedSearch(4, size);
+            int res = boundedSearch(4, searchSize);
             if (res > 0) {
                 printSolution("First of four is %d", res);
                 return;
             }
-            size *= 10;
+            searchSize *= 10;
         }
     }
 }
